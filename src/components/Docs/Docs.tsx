55f6020e48d65f395a18d1519a1d68ed6e886311
Bulrock <maksim.volozhinskij@gmail.com>
2023-05-01 00:14:37 +0200

    feat: implement sidebar for DOCS

--- src/components/Docs/Docs.tsx
+++ src/components/Docs/Docs.tsx
@@ -1,11 +1,25 @@
 import HeaderDocs from './HeaderDocs';
 import MainDocs from './MainDocs';
 import { DocsType } from '../../types/docs';
-import classes from './docs.module.css';
+import styles from './docs.module.css';
+import { useState } from 'react';
 
 export default function Docs({ schema }: DocsType) {
+  const [active, setActive] = useState(false);
+
+  const handleLableClick = () => {
+    if (!active) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
+
   return (
-    <div className={classes.wrapper}>
+    <div className={active ? styles.docsVisible : styles.docsInvisible}>
+      <div onClick={handleLableClick} className={styles.lable}>
+        DOCS
+      </div>
       <HeaderDocs header={''} />
       <MainDocs schema={schema} />
     </div>
