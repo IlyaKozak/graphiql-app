2c17129dd1a83ce0b99f9f19956aadacb8b33a2a
Bulrock <maksim.volozhinskij@gmail.com>
2023-04-30 21:19:21 +0200

    feat: implement responsive styles for Response, Editor and Variables sections of the playground

--- src/components/Docs/Docs.tsx
+++ src/components/Docs/Docs.tsx
@@ -1,10 +1,11 @@
 import HeaderDocs from './HeaderDocs';
 import MainDocs from './MainDocs';
 import { DocsType } from '../../types/docs';
+import classes from './docs.module.css';
 
 export default function Docs({ schema }: DocsType) {
   return (
-    <div className="">
+    <div className={classes.wrapper}>
       <HeaderDocs header={''} />
       <MainDocs schema={schema} />
     </div>
