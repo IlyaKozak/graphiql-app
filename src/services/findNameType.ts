fde224f29c75163904a07bc04700ba203ea63ee5
Siaw4ik <peretsviach@gmail.com>
2023-05-03 14:16:11 +0200

    refactor: linter fixes

--- src/services/findNameType.ts
+++ src/services/findNameType.ts
@@ -1,24 +1,24 @@
 import { __Type } from '@/types/schema';
 
-export function findNameType(key: string,obj: __Type, arr: String[] = []): string | null {
+export function findNameType(key: string, obj: __Type, arr: String[] = []): string | null {
   if (obj.name !== null && obj.ofType === null) {
     let keyName = obj.name;
     let valueName = obj.name;
     arr.reverse().forEach((item) => {
-      if (item === "NON_NULL") {
-        valueName += '!'
-      } else if( item === 'LIST'){
-        valueName = '[' + valueName + ']'
+      if (item === 'NON_NULL') {
+        valueName += '!';
+      } else if (item === 'LIST') {
+        valueName = '[' + valueName + ']';
       }
-    })
-    if(key === 'key') {
-      return keyName
-    } else if(key === 'value') {
-      return valueName
+    });
+    if (key === 'key') {
+      return keyName;
+    } else if (key === 'value') {
+      return valueName;
     }
   } else if (obj.ofType && obj.name === null) {
-    arr.push(String(obj.kind))
+    arr.push(String(obj.kind));
     return findNameType(key, obj.ofType, arr);
-  } 
+  }
   return null;
 }
