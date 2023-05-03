1cce3e5ecc3df4928e63d9aa55704ab8e5cf1047
Bulrock <maksim.volozhinskij@gmail.com>
2023-05-03 16:08:20 +0200

    feat: implement variables for graphiQL request

--- src/services/GraphiQLService.ts
+++ src/services/GraphiQLService.ts
@@ -4,41 +4,20 @@ export default async function graphiQLService(
   variables?: string | null,
   headers?: string | null
 ): Promise<string> {
+  let variablesObject;
+  if (variables) {
+    variablesObject = JSON.parse(variables);
+  }
+
   const response = await fetch(endpoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
     },
-    body: JSON.stringify({ query }),
+    body: JSON.stringify({ query: query, variables: variablesObject ? variablesObject : {} }),
   });
 
   const responseJSON = await response.json();
   return responseJSON;
 }
-
-// export default async function graphiQLService(
-//   endpoint: string,
-//   query: string | null,
-//   variables?: string | null,
-//   headers?: string | undefined
-// ): Promise<string> {
-//   const requestHeaders: HeadersInit = new Headers(
-//     JSON.parse(headers || '{"Content-Type":"application/json"}')
-//   );
-
-//   console.log(Object.fromEntries(requestHeaders));
-//   const header = Object.fromEntries(requestHeaders);
-
-//   const response = await fetch(endpoint, {
-//     method: 'POST',
-//     headers: header,
-//     body: JSON.stringify({
-//       query: { query },
-//       variables: { variables },
-//     }),
-//   });
-
-//   const responseJSON = await response.json();
-//   return responseJSON;
-// }
